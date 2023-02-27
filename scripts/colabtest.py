94a382a30ca36492d05fa444854cc2ccf37ed58d
camenduru <54370274+camenduru@users.noreply.github.com>
2023-02-27 03:02:42 +0300

    Update colabtest.py

--- scripts/colabtest.py
+++ scripts/colabtest.py
@@ -25,12 +25,11 @@ def run(command):
 
 def on_ui_tabs():
     with gr.Blocks() as colabtest:
-      with gr.Group():
-        b1=gr.Button()
-        command = gr.Textbox(max_lines=1, placeholder="command")
-        text_out = gr.Textbox()
-        b2=gr.Button()
-        b1.click(run, [command], [text_out])
-        b2.click(counter, [], [text_out])
-    return (colabtest, "colabtest", "colabtest")
+      b1=gr.Button()
+      command = gr.Textbox(max_lines=1, placeholder="command")
+      text_out = gr.Textbox()
+      b2=gr.Button()
+      b1.click(run, [command], [text_out])
+      b2.click(counter, [], [text_out])
+      return (colabtest, "colabtest", "colabtest")
 script_callbacks.on_ui_tabs(on_ui_tabs)
