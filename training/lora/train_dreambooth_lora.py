4dd759dff113d02df98730d0e4d6d32fab7f7abe
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-02 00:04:39 +0300

    test

--- training/lora/train_dreambooth_lora.py
+++ training/lora/train_dreambooth_lora.py
@@ -387,8 +387,8 @@ def parse_args(input_args=None):
     else:
         args = parser.parse_args()
 
-    if '-h' in args or '--help' in args:
-        parser.print_help()
+    # if '-h' in args or '--help' in args:
+    #     parser.print_help()
 
     env_local_rank = int(os.environ.get("LOCAL_RANK", -1))
     if env_local_rank != -1 and env_local_rank != args.local_rank:
