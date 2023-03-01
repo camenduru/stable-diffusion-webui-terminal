4b563f264173f2585c6366a7c993f781e89b4430
camenduru <54370274+camenduru@users.noreply.github.com>
2023-03-02 00:28:49 +0300

    common and all arguments

--- scripts/terminal.py
+++ scripts/terminal.py
@@ -52,7 +52,7 @@ def on_ui_tabs():
         with gr.Tab("Training"):
             with gr.Tab("Train Dreambooth"):
                 with gr.Box():
-                    with gr.Accordion("Train Dreambooth Arguments", open=False):
+                    with gr.Accordion("Train Dreambooth Common Arguments", open=False):
                         gr.Markdown(
                         """
                         ```py
@@ -258,7 +258,7 @@ def on_ui_tabs():
                     btn_run_live.click(run_live, inputs=command, outputs=out_text, show_progress=False)
             with gr.Tab("Train LoRA"):
                 with gr.Box():
-                    with gr.Accordion("Train Lora Arguments", open=False):
+                    with gr.Accordion("Train Lora Common Arguments", open=False):
                         gr.Markdown(
                         """
                         ```py
@@ -284,6 +284,159 @@ def on_ui_tabs():
                         --class_prompt="person" \\
                         --seed=69 \\
                         --num_class_images=12 \\ \n
+                        ```
+                        """)
+                    with gr.Accordion("Train Lora All Arguments", open=False):
+                        gr.Markdown(
+                        """
+                        -h, --help            show this help message and exit
+                        --pretrained_model_name_or_path PRETRAINED_MODEL_NAME_OR_PATH
+                                                Path to pretrained model or model identifier from
+                                                huggingface.co/models.
+                        --revision REVISION   Revision of pretrained model identifier from
+                                                huggingface.co/models.
+                        --tokenizer_name TOKENIZER_NAME
+                                                Pretrained tokenizer name or path if not the same as
+                                                model_name
+                        --instance_data_dir INSTANCE_DATA_DIR
+                                                A folder containing the training data of instance
+                                                images.
+                        --class_data_dir CLASS_DATA_DIR
+                                                A folder containing the training data of class images.
+                        --instance_prompt INSTANCE_PROMPT
+                                                The prompt with identifier specifying the instance
+                        --class_prompt CLASS_PROMPT
+                                                The prompt to specify images in the same class as
+                                                provided instance images.
+                        --validation_prompt VALIDATION_PROMPT
+                                                A prompt that is used during validation to verify that
+                                                the model is learning.
+                        --num_validation_images NUM_VALIDATION_IMAGES
+                                                Number of images that should be generated during
+                                                validation with `validation_prompt`.
+                        --validation_epochs VALIDATION_EPOCHS
+                                                Run dreambooth validation every X epochs. Dreambooth
+                                                validation consists of running the prompt
+                                                `args.validation_prompt` multiple times:
+                                                `args.num_validation_images`.
+                        --with_prior_preservation
+                                                Flag to add prior preservation loss.
+                        --prior_loss_weight PRIOR_LOSS_WEIGHT
+                                                The weight of prior preservation loss.
+                        --num_class_images NUM_CLASS_IMAGES
+                                                Minimal class images for prior preservation loss. If
+                                                there are not enough images already present in
+                                                class_data_dir, additional images will be sampled with
+                                                class_prompt.
+                        --output_dir OUTPUT_DIR
+                                                The output directory where the model predictions and
+                                                checkpoints will be written.
+                        --seed SEED           A seed for reproducible training.
+                        --resolution RESOLUTION
+                                                The resolution for input images, all the images in the
+                                                train/validation dataset will be resized to this
+                                                resolution
+                        --center_crop         Whether to center crop the input images to the
+                                                resolution. If not set, the images will be randomly
+                                                cropped. The images will be resized to the resolution
+                                                first before cropping.
+                        --train_batch_size TRAIN_BATCH_SIZE
+                                                Batch size (per device) for the training dataloader.
+                        --sample_batch_size SAMPLE_BATCH_SIZE
+                                                Batch size (per device) for sampling images.
+                        --num_train_epochs NUM_TRAIN_EPOCHS
+                        --max_train_steps MAX_TRAIN_STEPS
+                                                Total number of training steps to perform. If
+                                                provided, overrides num_train_epochs.
+                        --checkpointing_steps CHECKPOINTING_STEPS
+                                                Save a checkpoint of the training state every X
+                                                updates. These checkpoints can be used both as final
+                                                checkpoints in case they are better than the last
+                                                checkpoint, and are also suitable for resuming
+                                                training using `--resume_from_checkpoint`.
+                        --checkpoints_total_limit CHECKPOINTS_TOTAL_LIMIT
+                                                Max number of checkpoints to store. Passed as
+                                                `total_limit` to the `Accelerator`
+                                                `ProjectConfiguration`. See Accelerator::save_state ht
+                                                tps://huggingface.co/docs/accelerate/package_reference
+                                                /accelerator#accelerate.Accelerator.save_state for
+                                                more docs
+                        --resume_from_checkpoint RESUME_FROM_CHECKPOINT
+                                                Whether training should be resumed from a previous
+                                                checkpoint. Use a path saved by
+                                                `--checkpointing_steps`, or `"latest"` to
+                                                automatically select the last available checkpoint.
+                        --gradient_accumulation_steps GRADIENT_ACCUMULATION_STEPS
+                                                Number of updates steps to accumulate before
+                                                performing a backward/update pass.
+                        --gradient_checkpointing
+                                                Whether or not to use gradient checkpointing to save
+                                                memory at the expense of slower backward pass.
+                        --learning_rate LEARNING_RATE
+                                                Initial learning rate (after the potential warmup
+                                                period) to use.
+                        --scale_lr            Scale the learning rate by the number of GPUs,
+                                                gradient accumulation steps, and batch size.
+                        --lr_scheduler LR_SCHEDULER
+                                                The scheduler type to use. Choose between ["linear",
+                                                "cosine", "cosine_with_restarts", "polynomial",
+                                                "constant", "constant_with_warmup"]
+                        --lr_warmup_steps LR_WARMUP_STEPS
+                                                Number of steps for the warmup in the lr scheduler.
+                        --lr_num_cycles LR_NUM_CYCLES
+                                                Number of hard resets of the lr in
+                                                cosine_with_restarts scheduler.
+                        --lr_power LR_POWER   Power factor of the polynomial scheduler.
+                        --dataloader_num_workers DATALOADER_NUM_WORKERS
+                                                Number of subprocesses to use for data loading. 0
+                                                means that the data will be loaded in the main
+                                                process.
+                        --use_8bit_adam       Whether or not to use 8-bit Adam from bitsandbytes.
+                        --adam_beta1 ADAM_BETA1
+                                                The beta1 parameter for the Adam optimizer.
+                        --adam_beta2 ADAM_BETA2
+                                                The beta2 parameter for the Adam optimizer.
+                        --adam_weight_decay ADAM_WEIGHT_DECAY
+                                                Weight decay to use.
+                        --adam_epsilon ADAM_EPSILON
+                                                Epsilon value for the Adam optimizer
+                        --max_grad_norm MAX_GRAD_NORM
+                                                Max gradient norm.
+                        --push_to_hub         Whether or not to push the model to the Hub.
+                        --hub_token HUB_TOKEN
+                                                The token to use to push to the Model Hub.
+                        --hub_model_id HUB_MODEL_ID
+                                                The name of the repository to keep in sync with the
+                                                local `output_dir`.
+                        --logging_dir LOGGING_DIR
+                                                [TensorBoard](https://www.tensorflow.org/tensorboard)
+                                                log directory. Will default to
+                                                *output_dir/runs/**CURRENT_DATETIME_HOSTNAME***.
+                        --allow_tf32          Whether or not to allow TF32 on Ampere GPUs. Can be
+                                                used to speed up training. For more information, see h
+                                                ttps://pytorch.org/docs/stable/notes/cuda.html#tensorf
+                                                loat-32-tf32-on-ampere-devices
+                        --report_to REPORT_TO
+                                                The integration to report the results and logs to.
+                                                Supported platforms are `"tensorboard"` (default),
+                                                `"wandb"` and `"comet_ml"`. Use `"all"` to report to
+                                                all integrations.
+                        --mixed_precision {no,fp16,bf16}
+                                                Whether to use mixed precision. Choose between fp16
+                                                and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10.and
+                                                an Nvidia Ampere GPU. Default to the value of
+                                                accelerate config of the current system or the flag
+                                                passed with the `accelerate.launch` command. Use this
+                                                argument to override the accelerate config.
+                        --prior_generation_precision {no,fp32,fp16,bf16}
+                                                Choose prior generation precision between fp32, fp16
+                                                and bf16 (bfloat16). Bf16 requires PyTorch >= 1.10.and
+                                                an Nvidia Ampere GPU. Default to fp16 if a GPU is
+                                                available else fp32.
+                        --local_rank LOCAL_RANK
+                                                For distributed training: local_rank
+                        --enable_xformers_memory_efficient_attention
+                        Whether or not to use xformers.
                         ```
                         """)
                     train_lora_command = """python /content/stable-diffusion-webui/extensions/stable-diffusion-webui-terminal/training/lora/train_dreambooth_lora.py \\
